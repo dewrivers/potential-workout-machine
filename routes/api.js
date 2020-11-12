987b0b61b5b7dfa182a485217434002a9d465b2e
Rocio Rios <rrocio46@yahoo.es>
2020-11-12 18:28:21 -0500

    fixing bugs

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,21 @@
 const express = require('express');
-var db =require('../models');
+var Workout =require('../models/workout.js');
 var router =  express.Router();
 
- 
+router.post("/api/workouts", function(req, res){
+    console.log("WORKOUT POST API ROUTE HIT", req.body)
+    Workout.create({})
+        .then(data => {
+        res.json(data);
+    })
+    .catch(err => {res.json(err)});
+});
 
     router.put("/api/workouts/:id", function(req, res){
         var exerciseId = req.params.id;
         console.log("req setup", req.params.id);
         var exerciseData = req.body;
-        db.Workout.findByIdAndUpdate(
+        Workout.findByIdAndUpdate(
             exerciseId,
             {$push: {exercises: exerciseData}},
             {new:true, runValidators: true}
@@ -25,7 +32,7 @@ var router =  express.Router();
     // GET route for workouts data
     router.get("/api/workouts/", function(req, res){
     console.log("GET exercise data", req.params)
-    db.Workout.find()
+    Workout.find()
     .then(data => {
         res.json(data);
     })
@@ -35,19 +42,12 @@ var router =  express.Router();
 });
 
 // POST route for workouts data
-router.post("/api/workouts", function(req, res){
-    console.log("WORKOUT POST API ROUTE HIT", req.body)
-    db.Workout.create({})
-        .then(data => {
-        res.json(data);
-    })
-    .catch(err => {res.json(err)});
-});
+
 
 // GET route for stats data
 router.get("/api/workouts/range", function(req, res){
-    //("STATS GET ROUTE HIT", req.body)
-    db.Workout.find({}).limit(8)
+    
+    Workout.find({}).limit(8)
     .then(data => {res.json(data)})
     .catch(err => {
         console.log(err)
